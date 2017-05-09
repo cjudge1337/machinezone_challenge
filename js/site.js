88ae25d6dab0db2444a9f3e71f9396978fd4730c
Carson Judge <cjudge1337@gmail.com>
2017-05-08 17:58:36 -0700

    Timer working

--- js/site.js
+++ js/site.js
@@ -1,23 +1,57 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  // let myData = JSON.parse(data);
-  // console.log(data);
+  const saleData = JSON.parse(data);
+
+  load(saleData);
+  countdownTimer(saleData.saleEndTimestamp, 'sale-timer')
 });
 
 
-function load() {
-	var mydata = JSON.parse(data);
-	console.log(mydata);
+function load(saleData) {
+  window.data = saleData;
+
+	document.getElementById('sale-header-image').src = saleData.headerImage;
+  document.getElementById('blue-image').src = saleData.salesItems[0].image;
+  document.getElementById('jumper-image').src = saleData.salesItems[1].image;
+  document.getElementById('oliver-image').src = saleData.salesItems[2].image;
+  document.getElementById('two-face-image').src = saleData.salesItems[3].image;
+
+
 }
-// function readTextFile(file, callback) {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, false);
-//     rawFile.onreadystatechange = function() {
-//         if (rawFile.readyState === 4 && rawFile.status == "200") {
-//             callback(rawFile.responseText);
-//         }
-//     }
-//     rawFile.send(null);
-// }
+
+function countdownTimer(date, id) {
+  let end = new Date(date);
+
+  let _second = 1000;
+  let _minute = _second * 60;
+  let _hour = _minute * 60;
+  let _day = _hour * 24;
+  let timer;
+
+  function showRemaining() {
+    let now = new Date();
+    let distance = end - now;
+
+    if (distance < 0) {
+      clearInterval(timer);
+      document.getElementById(id).innerHTML = 'EXPIRED!';
+      return;
+    }
+
+    let days = Math.floor(distance / _day);
+    let hours = Math.floor((distance % _day) / _hour);
+    let minutes = Math.floor((distance % _hour) / _minute);
+    let seconds = Math.floor((distance % _minute) / _second);
+    let output = "This sale ends in ";
+
+    output += days + ' days ';
+    output += hours + ' hrs ';
+    output += minutes + ' mins ';
+    output += seconds + ' secs';
+
+    document.getElementById(id).innerHTML = output;
+  }
+
+  timer = setInterval(showRemaining, 1000);
+}
